cee827544ab7e3a7f4000efc5ad64ab582f06f91
MattIPv4 <me@mattcowley.co.uk>
2021-01-08 23:45:43 +0000

    Replace zombie with puppeteer

--- monitors/botblock.js
+++ monitors/botblock.js
@@ -1,19 +1,25 @@
-const Browser = require('zombie');
-Browser.localhost('botblock.org');
+const puppeteer = require('puppeteer');
+const assert = require('assert').strict;
+const fetch = require('node-fetch');
 
 module.exports = () => Promise.all([
-    new Promise(resolve => {
-        const browser = new Browser();
-        browser.visit('/', () => {
-            browser.assert.text('.hero h1', 'Discord Bot Lists? We have them all.');
-            resolve();
-        });
-    }),
-    new Promise(resolve => {
-        const browser = new Browser();
-        browser.visit('/api/lists', () => {
-            browser.assert.success();
-            resolve();
-        });
-    }),
+    (async () => {
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        await page.goto('https://botblock.org');
+
+        // Check the heading is there
+        const heading = await page.$eval('.hero h1', e => e.textContent);
+        assert.equal(heading.trim().replace(/(\s){2,}/g, '$1'), 'Discord Bot Lists? We have them all.');
+
+        await browser.close();
+    })(),
+    (async () => {
+        const res = await fetch('https://botblock.org/api/lists');
+        if (!res.ok) throw new Error(`HTTP request failed: ${res.status} ${res.statusText}`);
+
+        const data = await res.json();
+        if (data === null || typeof data !== 'object')
+            throw new Error(`Unexpected endpoint response: ${JSON.stringify(data)}`);
+    })(),
 ]);
--- monitors/personal-website.js
+++ monitors/personal-website.js
@@ -1,10 +1,14 @@
-const Browser = require('zombie');
-Browser.localhost('mattcowley.co.uk');
+const puppeteer = require('puppeteer');
+const assert = require('assert').strict;
 
-module.exports = () => new Promise(resolve => {
-    const browser = new Browser();
-    browser.visit('/', () => {
-        browser.assert.text('.content h1', 'Matt (IPv4) Cowley');
-        resolve();
-    });
-});
+module.exports = async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('https://mattcowley.co.uk');
+
+    // Check the heading is there
+    const heading = await page.$eval('.content h1', e => e.textContent);
+    assert(heading.trim().replace(/(\s){2,}/g, '$1').includes('Matt (IPv4) Cowley'));
+
+    await browser.close();
+};
