2056bab942d6923ab5e41f51e313547f2f63a149
MattIPv4 <me@mattcowley.co.uk>
2021-01-08 21:43:19 +0000

    Consola makes life look pretty

--- src/monitor.js
+++ src/monitor.js
@@ -1,30 +1,43 @@
-const monitorName = process.argv[2];
+const consola = require('consola');
 
-const getMonitor = name => require(`../monitors/${name}.js`);
-
+// Show any error and then exit
 const error = (msg, err) => {
-    console.error(msg);
-    if (err) console.error(err);
+    consola.fatal(msg);
+    if (err) consola.error(err);
     process.exit(1);
 };
 
-const main = async () => {
-    console.log(`Loading monitor ${monitorName}...`);
-    let monitorFunc;
+// Import a monitor module
+const loadMonitor = name => {
+    consola.info(`Loading monitor ${name}...`);
     try {
-        monitorFunc = getMonitor(monitorName);
-    } catch (_) {
-        return error('Failed to load');
+        const monitorFunc = require(`../monitors/${name}.js`);
+        consola.success('Loaded!');
+        return monitorFunc;
+    } catch (err) {
+        return error('Failed to load', err);
     }
+};
 
-    console.log(`Running monitor ${monitorName}...`);
+// Run a monitor module
+const runMonitor = async (name, func) => {
+    consola.info(`Running monitor ${name}...`);
     try {
-        await monitorFunc();
+        await func();
+        consola.success('Passed!');
     } catch (err) {
         return error('Failed to run', err);
     }
+};
 
-    console.log('Passed');
+// Import & run the monitor
+const main = async () => {
+    if (!process.argv || process.argv.length < 3)
+        return error('Specify monitor name as argument');
+
+    const monitorName = process.argv[2];
+    const monitorFunc = loadMonitor(monitorName);
+    await runMonitor(monitorName, monitorFunc);
 };
 
 main().then(() => {}).catch(() => process.exit(1));
