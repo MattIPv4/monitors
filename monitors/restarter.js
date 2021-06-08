2c77cdc2f116586ed235ff5ea55ab35ad7074d0b
MattIPv4 <me@mattcowley.co.uk>
2021-06-08 12:46:22 +0100

    Add monitor for restarter website

---
+++ monitors/restarter.js
@@ -0,0 +1,29 @@
+const puppeteer = require('puppeteer');
+const assert = require('assert').strict;
+
+module.exports = async () => {
+    const browser = await puppeteer.launch();
+
+    try {
+        const page = await browser.newPage();
+
+        // Check the heading is there
+        await page.goto('https://restarter.mattcowley.co.uk');
+        const headingHomepage = await page.$eval('.content h1', e => e.textContent);
+        assert.equal(headingHomepage.trim(), 'Restarter v3');
+
+        await page.goto('https://restarter.mattcowley.co.uk/docs/');
+        const headingDocs = await page.$eval('.content h1', e => e.textContent);
+        assert.equal(headingDocs.trim(), 'Restarter v3 Docs');
+
+    } catch (e) {
+        // Close the browser before error-ing
+        await browser.close();
+
+        // Re-throw error
+        throw e;
+    }
+
+    // Tests passed, close browser
+    await browser.close();
+};
