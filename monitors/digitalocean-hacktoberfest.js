bee2826ba86808aac0d3d880ffe794642841c32e
MattIPv4 <me@mattcowley.co.uk>
2021-12-15 17:50:17 +0000

    Update Hacktoberfest monitor

--- monitors/digitalocean-hacktoberfest.js
+++ monitors/digitalocean-hacktoberfest.js
@@ -9,8 +9,12 @@ module.exports = async () => {
         await page.goto('https://hacktoberfest.digitalocean.com/');
 
         // Check the heading is there
-        const heading = await page.$eval('p:first-of-type', e => e.textContent);
-        assert.equal(heading.trim().replace(/(\s){2,}/g, '$1'), 'Open source is changing the world – one contribution at a time.');
+        // const heading = await page.$eval('p:first-of-type', e => e.textContent);
+        // assert.equal(heading.trim().replace(/(\s){2,}/g, '$1'), 'Open source is changing the world – one contribution at a time.');
+
+        // Check the blog link is there
+        const blogLink = await page.$('a[href="https://www.digitalocean.com/blog/hacktoberfest-2021-recap/"]');
+        assert.notEqual(blogLink, null);
 
     } catch (e) {
         // Close the browser before error-ing
