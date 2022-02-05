f16dd46caa5048e3ede92ace7ba8ef0597b9db24
MattIPv4 <me@mattcowley.co.uk>
2022-02-05 03:37:47 +0000

    Add digitalocean-spf monitor

---
+++ monitors/digitalocean-spf.js
@@ -0,0 +1,26 @@
+const assert = require('assert').strict;
+const browserPage = require('../utils/browser-page');
+
+module.exports = () => browserPage('https://www.digitalocean.com/community/tools/spf', async page => {
+    // Check the heading is there
+    const heading = await page.$eval('.do-bulma .landing h1', e => e.textContent);
+    assert.equal(heading.trim(), 'SPF Explainer');
+
+    // Click on the domain input box and type domain
+    await page.focus('.do-bulma .landing .input');
+    await page.keyboard.type('digitalocean.com');
+
+    // Click the search button
+    await page.click('.do-bulma .landing .button');
+
+    // Allow the results to load
+    await page.waitForTimeout(1500);
+
+    // Check results are present
+    const resultsHeading = await page.$('.do-bulma .container > div > div:nth-of-type(1)');
+    assert.notEqual(resultsHeading, null);
+    const resultsHeadingText = await resultsHeading.evaluate(e => e.textContent);
+    assert.equal(resultsHeadingText.trim(), 'SPF records for digitalocean.com:');
+    const resultsDiv = await page.$('.do-bulma .container > div > div:nth-of-type(2)');
+    assert.notEqual(resultsDiv, null);
+});
