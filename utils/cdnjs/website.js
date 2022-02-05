851ef3258622265cb7581ee6d50e1e246d681fe1
MattIPv4 <me@mattcowley.co.uk>
2022-02-05 17:29:15 +0000

    Improve cdnjs website test

--- utils/cdnjs/website.js
+++ utils/cdnjs/website.js
@@ -6,17 +6,15 @@ module.exports = host => browserPage(`https://${host}/`, async page => {
     const heading = await page.$eval('.landing h1', e => e.textContent);
     assert.equal(heading.trim().replace(/(\s){2,}/g, '$1'), 'Simple. Fast. Reliable. Content delivery at its finest.');
 
-    // Click on something other than the search box
-    await page.click('.landing h1');
-    await page.waitForTimeout(1500);
-    await page.click('.landing h1');
-    await page.waitForTimeout(1500);
+    // Wait for Algolia to load
+    await page.waitForNetworkIdle();
 
-    // Click back on the search box to trigger hits to show
+    // Ensure we're in the search box, and type a query
     await page.click('.landing .ais-SearchBox-input');
-    await page.waitForTimeout(1500);
-    await page.click('.landing .ais-SearchBox-input');
-    await page.waitForTimeout(1500);
+    await page.keyboard.type('react');
+
+    // Wait for Algolia to load
+    await page.waitForNetworkIdle();
 
     // Ensure the search box shows some hits
     const hits = await page.$('.landing .ais-Hits');
