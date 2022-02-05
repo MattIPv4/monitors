9b3c68e7d53a62071049b095fe5748c947274e0d
MattIPv4 <me@mattcowley.co.uk>
2022-02-05 16:45:26 +0000

    Set no-cache and check response status in browserPage

--- utils/browser-page.js
+++ utils/browser-page.js
@@ -3,8 +3,31 @@ const puppeteer = require('puppeteer');
 module.exports = async (url, callback) => {
     const browser = await puppeteer.launch({ defaultViewport: { width: 1920, height: 1080 } });
     try {
-        const page = await browser.newPage();
-        const response = await page.goto(url);
+        const pages = await browser.pages();
+        const page = pages.length ? pages[0] : await browser.newPage();
+
+        // Enable request interception
+        await page.setRequestInterception(true);
+
+        // Add no-cache for the navigation requests
+        page.on('request', request => {
+            // Do nothing in case of non-navigation requests.
+            if (!request.isNavigationRequest()) {
+                request.continue();
+                return;
+            }
+
+            // Add a new header for navigation request.
+            const headers = request.headers();
+            headers['Pragma'] = 'no-cache';
+            headers['Cache'] = 'no-cache';
+            headers['Cache-Control'] = 'no-cache';
+            request.continue({ headers });
+        });
+
+        // Make the page request, check response, and run the callback
+        const response = await page.goto(url, { waitUntil: 'domcontentloaded' });
+        if (!response.ok()) throw new Error(`HTTP request failed: ${response.status()} ${response.statusText()}`);
         await callback(page, response);
     } finally {
         // Close the browser
