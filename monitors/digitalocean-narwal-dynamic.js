09aa8b9564fdd7e013baae0678ba908d5dcea978
MattIPv4 <me@mattcowley.co.uk>
2022-02-05 16:35:22 +0000

    Add auth checks to digitalocean-narwal-dynamic/regeneration

--- monitors/digitalocean-narwal-dynamic.js
+++ monitors/digitalocean-narwal-dynamic.js
@@ -1,3 +1,24 @@
+const assert = require('assert').strict;
 const fetchHealth = require('../utils/fetch-health');
+const fetchUncached = require('../utils/fetch-uncached');
 
-module.exports = () => fetchHealth('https://www.digitalocean.com/api/dynamic-content/health');
+module.exports = () => Promise.all([
+    fetchHealth('https://www.digitalocean.com/api/dynamic-content/health'),
+    (async () => {
+        // Fetch and check response code
+        const res = await fetchUncached('https://www.digitalocean.com/api/dynamic-content/v1/tutorials/react-axios-react');
+        assert.equal(res.status, 401);
+
+        // Get the error data
+        const data = await res.json();
+
+        // Check we get the expected error keys
+        assert.equal(Object.keys(data).length, 2);
+        assert('code' in data);
+        assert('message' in data);
+
+        // Check we get the expected error message
+        assert.equal(data.code, 'InvalidCredentials');
+        assert.equal(data.message, 'Expected Authorization header');
+    })(),
+]);
--- utils/fetch-uncached.js
+++ utils/fetch-uncached.js
@@ -1,14 +1,16 @@
 const fetch = require('node-fetch');
 
-module.exports = url => {
+module.exports = (url, opts = {}) => {
     const controller = new AbortController();
     const timeout = setTimeout(() => {
         controller.abort()
     }, 30000);
 
     return fetch(url, {
+        ...opts,
         signal: controller.signal,
         headers: {
+            ...(opts || {}).headers,
             'Pragma': 'no-cache',
             'Cache': 'no-cache',
             'Cache-Control': 'no-cache',
