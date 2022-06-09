cd4aae302fd86be3af04558b7c9f7b2c068e91d1
MattIPv4 <me@mattcowley.co.uk>
2022-06-09 23:29:08 +0100

    Add some new monitors for DigitalOcean redirects

---
+++ monitors/digitalocean-alligator-redirect.js
@@ -0,0 +1,8 @@
+import { strict as assert } from 'assert';
+import fetchUncached from '../utils/fetch-uncached.js';
+
+export default async () => {
+    const res = await fetchUncached('https://alligator.io', { redirect: 'manual' });
+    assert.equal(res.status, 301);
+    assert.equal(res.headers.get('location'), 'https://www.digitalocean.com/community/');
+};
