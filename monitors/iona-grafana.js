d5d960a5adf659209da61575d8b4773ce1b23187
MattIPv4 <me@mattcowley.co.uk>
2022-07-03 18:07:26 +0100

    Update iona grafana monitor

--- monitors/iona-grafana.js
+++ monitors/iona-grafana.js
@@ -10,9 +10,18 @@ export default () => Promise.all([
         assert.equal(chain.length, 1);
         assert.equal(chain[0].frame().url(), 'http://iona.s.mattcowley.co.uk:8080/login');
 
+        // Wait for the login page to load
+        await page.waitForSelector('h1', { timeout: 5000 });
+
         // Check the login heading is there
         const heading = await page.$eval('h1', e => e.textContent);
         assert.equal(heading.trim(), 'Welcome to Grafana');
+
+        // Check the login form is there
+        const email = await page.$('input[aria-label="Username input field"]');
+        assert.notEqual(email, null);
+        const pswd = await page.$('input[aria-label="Password input field"]');
+        assert.notEqual(pswd, null);
 
         // Check the login button is there
         const button = await page.$eval('button[aria-label="Login button"]', e => e.textContent);
