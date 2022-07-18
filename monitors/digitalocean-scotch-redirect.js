dc05d01b545adb444a64ffde9f761a2ea97948dd
MattIPv4 <me@mattcowley.co.uk>
2022-07-18 18:34:01 +0100

    Fix scotch redirect

--- monitors/digitalocean-scotch-redirect.js
+++ monitors/digitalocean-scotch-redirect.js
@@ -4,5 +4,5 @@ import fetchUncached from '../utils/fetch-uncached.js';
 export default async () => {
     const res = await fetchUncached('https://scotch.io', { redirect: 'manual' });
     assert.equal(res.status, 301);
-    assert.equal(res.headers.get('location'), 'https://www.digitalocean.com/community');
+    assert.equal(res.headers.get('location'), 'https://www.digitalocean.com/community/');
 };
