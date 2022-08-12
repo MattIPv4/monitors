a31f6da08d9c1d39acad3b387ad894c74a1143fe
MattIPv4 <me@mattcowley.co.uk>
2022-08-12 21:51:32 +0100

    Remove tutorials endpoint from cdnjs API monitors

--- utils/cdnjs/api.js
+++ utils/cdnjs/api.js
@@ -2,6 +2,6 @@ import fetchJson from '../fetch-json';
 
 export default (host, lib = 'vue') => Promise.all([
     fetchJson(`https://${host}/libraries`),
-    fetchJson(`https://${host}/libraries/${lib}`),
-    fetchJson(`https://${host}/libraries/${lib}/tutorials`),
+    fetchJson(`https://${host}/libraries/${lib}`)
+        .then(({ version }) => fetchJson(`https://${host}/libraries/${lib}/${version}`)),
 ]);
