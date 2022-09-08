df5e1980693a0fd754b186497ebc42e813764125
MattIPv4 <me@mattcowley.co.uk>
2022-09-08 18:04:40 +0100

    Update Hacktoberfest redirect monitor status code

--- monitors/digitalocean-hacktoberfest-redirect.js
+++ monitors/digitalocean-hacktoberfest-redirect.js
@@ -3,6 +3,6 @@ import fetchUncached from '../utils/fetch-uncached.js';
 
 export default async () => {
     const res = await fetchUncached('https://hacktoberfest.digitalocean.com', { redirect: 'manual' });
-    assert.equal(res.status, 302);
+    assert.equal(res.status, 301);
     assert.equal(res.headers.get('location'), 'https://hacktoberfest.com/');
 };
