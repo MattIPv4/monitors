d23a72b7e3262a0d4442b5fcc5c9c0284e4eedf2
MattIPv4 <me@mattcowley.co.uk>
2022-09-08 18:01:30 +0100

    Update Hacktoberfest monitors

--- monitors/digitalocean-hacktoberfest-redirect.js
+++ monitors/digitalocean-hacktoberfest-redirect.js
@@ -2,7 +2,7 @@ import { strict as assert } from 'assert';
 import fetchUncached from '../utils/fetch-uncached.js';
 
 export default async () => {
-    const res = await fetchUncached('https://hacktoberfest.com', { redirect: 'manual' });
+    const res = await fetchUncached('https://hacktoberfest.digitalocean.com', { redirect: 'manual' });
     assert.equal(res.status, 302);
-    assert.equal(res.headers.get('location'), 'https://hacktoberfest.digitalocean.com/');
+    assert.equal(res.headers.get('location'), 'https://hacktoberfest.com/');
 };
--- monitors/digitalocean-hacktoberfest.js
+++ monitors/digitalocean-hacktoberfest.js
@@ -1,12 +1,8 @@
 import { strict as assert } from 'assert';
 import browserPage from '../utils/browser-page';
 
-export default () => browserPage('https://hacktoberfest.digitalocean.com/', async page => {
+export default () => browserPage('https://hacktoberfest.com/', async page => {
     // Check the heading is there
-    // const heading = await page.$eval('p:first-of-type', e => e.textContent);
-    // assert.equal(heading.trim().replace(/(\s){2,}/g, '$1'), 'Open source is changing the world – one contribution at a time.');
-
-    // Check the blog link is there
-    const blogLink = await page.$('a[href="https://www.digitalocean.com/blog/hacktoberfest-2021-recap/"]');
-    assert.notEqual(blogLink, null);
+    const heading = await page.$eval('h2:first-of-type', e => e.textContent);
+    assert.equal(heading.trim().replace(/(\s){2,}/g, '$1'), 'Prepare to Hack');
 });
