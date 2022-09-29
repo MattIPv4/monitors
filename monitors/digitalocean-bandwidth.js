86be40ab6bf2a1bc89989d82eb434b9a6379a154
MattIPv4 <me@mattcowley.co.uk>
2022-09-29 21:59:25 +0100

    Fix digitalocean-bandwidth monitor

--- monitors/digitalocean-bandwidth.js
+++ monitors/digitalocean-bandwidth.js
@@ -13,11 +13,11 @@ export default () => browserPage('https://www.digitalocean.com/community/tools/b
     // Find a new Droplet to add
     const [ dropletPanel ] = await page.$$('.picker .panel-list .panel.is-droplet');
     assert.notEqual(dropletPanel, null);
-    await page.evaluate(element => element.scrollIntoView(), dropletPanel);
+    await page.evaluate(element => element.scrollIntoView({ behavior: 'smooth', block: 'center' }), dropletPanel);
     await page.waitForTimeout(500);
 
     // Add the Droplet
     await dropletPanel.click();
     const updatedDroplets = await page.$$('.droplets .panel-list-vertical .panel.is-droplet');
     assert.equal(updatedDroplets.length, 2);
-});
+}, false, [ 'consent.trustarc.com' ]);
