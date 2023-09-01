66b5639e5e40d5ac09120662c5427711079257ab
MattIPv4 <me@mattcowley.co.uk>
2023-09-01 19:40:25 +0100

    Update Hacktoberfest monitor for new heading

--- monitors/digitalocean-hacktoberfest.js
+++ monitors/digitalocean-hacktoberfest.js
@@ -3,6 +3,6 @@ import browserPage from '../utils/browser-page';
 
 export default () => browserPage('https://hacktoberfest.com/', async page => {
     // Check the heading is there
-    const heading = await page.$eval('h2:first-of-type', e => e.textContent);
-    assert.equal(heading.trim().replace(/(\s){2,}/g, '$1'), 'Prepare to Hack');
+    const heading = await page.$eval('h1:first-of-type', e => e.textContent);
+    assert.equal(heading.trim().replace(/(\s){2,}/g, '$1'), 'Celebrate our 10th year supporting open source!');
 });
