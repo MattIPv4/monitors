6a3be10fe52f0f750320ed0c63d83add63ce1956
MattIPv4 <me@mattcowley.co.uk>
2023-09-04 00:44:39 +0100

    Reload monitors from disk when retrying

--- src/monitor.js
+++ src/monitor.js
@@ -11,7 +11,7 @@ const error = (msg, err) => {
 const loadMonitor = async name => {
     consola.info(`Loading monitor ${name}...`);
     try {
-        const { default: monitorFunc } = await import(`../monitors/${name}.js`);
+        const { default: monitorFunc } = await import(`../monitors/${name}.js?_=${Date.now()}`);
         consola.success('Loaded!');
         return monitorFunc;
     } catch (err) {
@@ -20,36 +20,36 @@ const loadMonitor = async name => {
 };
 
 // Run a monitor module
-const runMonitor = async (name, func) => {
+const runMonitor = async (name) => {
+    const func = await loadMonitor(name);
     consola.info(`Running monitor ${name}...`);
-    try {
-        await func();
-        consola.success('Passed!');
-        return;
-    } catch (err) {
-        consola.warn('Failed to run');
-        consola.warn(err);
-    }
-
-    consola.info(`Rerunning monitor ${name} in 30s...`);
-    await new Promise(resolve => setTimeout(resolve, 30000));
-    try {
-        await func();
-        consola.success('Passed!');
-    } catch (err) {
-        return error('Failed to run', err);
-    }
+    await func();
+    consola.success('Passed!');
 };
 
 // Import & run the monitor
 const main = async () => {
     if (!process.argv || process.argv.length < 3)
         return error('Specify monitor name as argument');
+    const name = process.argv[2];
 
-    const monitorName = process.argv[2];
-    const monitorFunc = await loadMonitor(monitorName);
-    await runMonitor(monitorName, monitorFunc);
+    // First attempt to run the monitor
+    try {
+        await runMonitor(name);
+        return;
+    } catch (err) {
+        consola.warn('Failed to run');
+        consola.warn(err);
+    }
+
+    // If that failed, wait 30s and try again
+    consola.info(`Rerunning monitor ${name} in 30s...`);
+    await new Promise(resolve => setTimeout(resolve, 30000));
+    try {
+        await runMonitor(name);
+    } catch (err) {
+        error('Failed to run', err);
+    }
 };
 
 main().then(() => {}).catch(() => process.exit(1));
-
