52bfc60b5cc0cafbb63927c0874fbcb0e6218e4b
MattIPv4 <me@mattcowley.co.uk>
2023-09-04 00:44:51 +0100

    Update Restarter monitor for new heading

--- monitors/restarter.js
+++ monitors/restarter.js
@@ -5,11 +5,11 @@ export default () => Promise.all([
     browserPage('https://restarter.mattcowley.co.uk', async page => {
         // Check the heading is there
         const headingHomepage = await page.$eval('.content h1', e => e.textContent);
-        assert.equal(headingHomepage.trim(), 'Restarter v3');
+        assert.equal(headingHomepage.trim(), 'Restarter');
     }),
     browserPage('https://restarter.mattcowley.co.uk/docs/', async page => {
         // Check the docs heading is there
         const headingDocs = await page.$eval('.content h1', e => e.textContent);
-        assert.equal(headingDocs.trim(), 'Restarter v3 Docs');
+        assert.equal(headingDocs.trim(), 'Restarter Docs');
     }),
 ]);
