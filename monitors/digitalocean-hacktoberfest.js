8a0983be81d219810b76265af05aaa825a941cd3
MattIPv4 <me@mattcowley.co.uk>
2023-11-01 14:30:15 +0000

    Update Hacktoberfest monitor to be more generic

--- monitors/digitalocean-hacktoberfest.js
+++ monitors/digitalocean-hacktoberfest.js
@@ -2,7 +2,16 @@ import { strict as assert } from 'node:assert';
 import browserPage from '../utils/browser-page';
 
 export default () => browserPage('https://hacktoberfest.com/', async page => {
-    // Check the heading is there
-    const heading = await page.$eval('h1:first-of-type', e => e.textContent);
-    assert.equal(heading.trim().replace(/(\s){2,}/g, '$1'), 'Celebrate our 10th year supporting open source!');
+    // Check page title
+    const year = new Date().getFullYear();
+    const title = await page.title();
+    assert(title.match(new RegExp(`^Hacktoberfest (${year}|${year - 1})$`)));
+
+    // Check the navigation is there
+    const [ participation ] = await page.$$('nav a:first-of-type');
+    assert.notEqual(participation, null);
+    const participationText = await page.evaluate(element => element.textContent.trim(), participation);
+    assert.equal(participationText, 'Participation');
+    const participationLink = await page.evaluate(element => element.href, participation);
+    assert.equal(participationLink, 'https://hacktoberfest.com/participation/');
 });
