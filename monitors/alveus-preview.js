e382794344812b97131c1305716de498db1aa959
MattIPv4 <me@mattcowley.co.uk>
2024-05-19 23:33:30 +0100

    Remove Alveus preview monitor

--- monitors/alveus-preview.js
+++
@@ -1,8 +0,0 @@
-import { strict as assert } from 'node:assert';
-import browserPage from '../utils/browser-page.js';
-
-export default () => browserPage('https://preview.alveus.gg', async page => {
-    // Check the heading is there
-    const heading = await page.$eval('h1', e => e.textContent);
-    assert(heading.trim().replace(/(\s){2,}/g, '$1').includes('Educating the World from the Web'));
-});
