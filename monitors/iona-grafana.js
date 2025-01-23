f65683f18f5da53a88dbd8f71df7c5437e980aea
MattIPv4 <me@mattcowley.co.uk>
2025-01-23 02:05:23 +0000

    Fix Iona Grafana monitor

--- monitors/iona-grafana.js
+++ monitors/iona-grafana.js
@@ -9,20 +9,20 @@ export default () => Promise.all([
         assert.equal(page.url(), 'http://iona.s.mattcowley.co.uk:8080/login');
 
         // Wait for the login page to load
-        await page.waitForSelector('h1', { timeout: 5000 });
+        await page.waitForSelector('h1', { timeout: 5000, visible: true });
 
         // Check the login heading is there
         const heading = await page.$eval('h1', e => e.textContent);
         assert.equal(heading.trim(), 'Welcome to Grafana');
 
         // Check the login form is there
-        const email = await page.$('input[aria-label="Username input field"]');
+        const email = await page.$('input::-p-aria("Email or username")');
         assert.notEqual(email, null);
-        const pswd = await page.$('input[aria-label="Password input field"]');
+        const pswd = await page.$('input::-p-aria("Password")');
         assert.notEqual(pswd, null);
 
         // Check the login button is there
-        const button = await page.$eval('button[aria-label="Login button"]', e => e.textContent);
-        assert.equal(button.trim(), 'Log in');
+        const login = await page.$('button::-p-aria("Log in")');
+        assert.notEqual(login, null);
     }),
 ]);
