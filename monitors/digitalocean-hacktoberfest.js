0c3c3471a8cf5b28b974de86489c52de8ee959c1
MattIPv4 <me@mattcowley.co.uk>
2025-05-19 21:05:44 +0100

    Fix DigitalOcean Hacktoberfest monitor

--- monitors/digitalocean-hacktoberfest.js
+++ monitors/digitalocean-hacktoberfest.js
@@ -6,12 +6,4 @@ export default () => browserPage('https://hacktoberfest.com/', async page => {
     const year = new Date().getFullYear();
     const title = await page.title();
     assert(title.match(new RegExp(`^Hacktoberfest (${year}|${year - 1})$`)));
-
-    // Check the navigation is there
-    const [ participation ] = await page.$$('nav a:first-of-type');
-    assert.notEqual(participation, null);
-    const participationText = await page.evaluate(element => element.textContent.trim(), participation);
-    assert.equal(participationText, 'Participation');
-    const participationLink = await page.evaluate(element => element.href, participation);
-    assert.equal(participationLink, 'https://hacktoberfest.com/participation/');
 });
