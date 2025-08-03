025c0ec847e991e5395143e5f4d63ee012e42926
MattIPv4 <me@mattcowley.co.uk>
2025-08-03 22:07:34 +0100

    Disable iona monitors

--- src/disabled.js
+++ src/disabled.js
@@ -1,2 +1,7 @@
 // Output a list of disabled monitor names
-console.log(JSON.stringify(["digitalocean-narwal-regeneration"]));
+console.log(JSON.stringify([
+  "digitalocean-narwal-regeneration",
+  "iona-postgres",
+  "iona-ssh",
+  "iona-grafana",
+]));
